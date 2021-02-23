1283664fcee607515b2c8d6a4189a6ad518872f7
novellcloud <78338386+novellcloud@users.noreply.github.com>
2021-02-23 14:39:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require('UserInfo.php');
     "device" : "<?= UserInfo::get_device();?>",
     "os" : "<?= UserInfo::get_os();?>",
     "browser" : "<?= UserInfo::get_browser();?>", 
-    "language" : "<?= UserInfo::get_language();?>", 
+    "language" : "<?= UserInfo::getLanguage();?>", 
     "latitude" : 37.7697, 
     "longitude" : -122.3933,
     "timezone" : "America/Los_Angeles"
