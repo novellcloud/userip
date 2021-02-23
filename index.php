95fbe6c4705853d8531da280256c6c243e88e853
novellcloud <78338386+novellcloud@users.noreply.github.com>
2021-02-23 14:31:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,12 @@ require('UserInfo.php');
 ?>
 {
     "ip" : "<?= UserInfo::get_ip();?>", 
-    "device" : <?= UserInfo::get_device();?> 
-    "os" : <?= UserInfo::get_os();?> 
-    "country" : "US", 
+    "device" : "<?= UserInfo::get_device();?>",
+    "os" : "<?= UserInfo::get_os();?>",
+    "browser" : "<?= UserInfo::get_browser();?>", 
     "postal" : "94107", 
     "latitude" : 37.7697, 
     "longitude" : -122.3933,
     "timezone" : "America/Los_Angeles"
 }
-			 "ip": <?= UserInfo::get_ip();?></td>
-			<td><?= UserInfo::get_device();?></td>
-			<td><?= UserInfo::get_os();?></td>
-			<td><?= UserInfo::get_browser();?></td>
 	
