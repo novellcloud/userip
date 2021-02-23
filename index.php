b2f12149d213a96ff6dc8331082bc790f715b9ed
novellcloud <78338386+novellcloud@users.noreply.github.com>
2021-02-23 14:14:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,7 @@
 require('UserInfo.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>UserInfo Demo</title>
-	<style>
-table {
-	margin-top: 20px;
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
 
-td, th {
-    border: 1px solid #dddddd;
-    text-align: center;
-    padding: 8px;
-}
-
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
-h2{font-family: sans-serif,'Helvetica';}
-</style>
 
 </head>
 <body>
